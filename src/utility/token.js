73ae482e08cf3d02788570669da192c63b6129c9
FrostDog <aleks.gubskyy@gmail.com>
2021-03-06 17:52:25 +0100

    Update token.js

--- src/utility/token.js
+++ src/utility/token.js
@@ -1,6 +1,6 @@
-const t1 = "e499de18e84d3"
-const t2 = "a6d3f05ecfea4b3"
-const t3 = "7cd4f6769c02"
+const t1 = "e499de18e84d3";
+const t2 = "a6d3f05ecfea4b3";
+const t3 = "7cd4f6769c02";
 
 const token = t1 + t2 + t3;
 
