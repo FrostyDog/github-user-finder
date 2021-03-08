41f10a5bfc56689c1e329840a505e6c7e04d8863
FrostDog <aleks.gubskyy@gmail.com>
2021-03-08 01:54:38 +0100

    Update token.js

--- src/utility/token.js
+++ src/utility/token.js
@@ -1,11 +1,3 @@
-const t1 = "e499de18e84d3";
-const t2 = "a6d3f05ecfea4b3";
-const t3 = "7cd4f6769c02";
-
-const token = t1 + t2 + t3;
-
-const token2 = process.env.REACT_APP_GH_TOKEN;
-
-console.log(token2);
+const token = process.env.REACT_APP_GH_TOKEN;
 
 export default token;
